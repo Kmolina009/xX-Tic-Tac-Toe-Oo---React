1a86bcaf5ae1516c1c67b723b51d39d8daf5bf9b
Kmolina009 <kevenmolina@gmail.com>
2024-06-29 20:31:48 -0400

    Created Square and Board Components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function Square(){
-  return <button className='square'>X</button>
+  return <button className='square'>1</button>
+}
+
+function Board(){
+  return (
+  <div>
+    <div>  
+      <Square />    
+      <Square />    
+      <Square />    
+    </div>
+    <div>  
+      <Square />    
+      <Square />    
+      <Square />    
+    </div>
+    <div>  
+      <Square />    
+      <Square />    
+      <Square />    
+    </div>
+  </div>
+  )
 }
 function App() {
   return (
     <div className="App">
-      <Square />
+      <Board />
       <header className="App-header">
       </header>
     </div>
