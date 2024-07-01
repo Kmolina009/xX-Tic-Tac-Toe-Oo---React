f79a99f83ccc09243ba83afdda6e2a1131f56a73
Kmolina009 <kevenmolina@gmail.com>
2024-06-30 21:22:59 -0400

    Wrote out the main point of lifting state from child to parent component and why components would share state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import { useState } from 'react';
-
 import logo from './logo.svg';
 import './App.css';
 
+//To check for a winnerm the Board would eed to somehow know the state of each of the 9 Square Component 
+// One could have the board ask each Square component for it's cuirrent State. - this is not recommented(convoluted, prone to bugs)
+//Instead the Board can tell each square what data it should display given a current State - You want the Parent component to store state
+
+
+// To collect data fron multiple children, or have two child components communicate with each other, declare the shared state in 
+// their parent component instead. The parent component can pass that state back down to the children via props. This keeps the 
+// child components in sync with each other and with their parents. 
+
+//Lifting state into a parent component is commen when React Components are being refactored.
+ 
 function Square({ value }){
    
   const [marking, setMarking] = useState(null);
