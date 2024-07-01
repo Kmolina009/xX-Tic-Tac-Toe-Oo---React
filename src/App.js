3f06482ff39755aaff87dee4b5f2e064c166f9a7
Kmolina009 <kevenmolina@gmail.com>
2024-06-30 22:00:06 -0400

    passing 'handle' prop into child components(Squares)

--- src/App.js
+++ src/App.js
@@ -12,32 +12,32 @@ import './App.css';
 // child components in sync with each other and with their parents. 
 
 //Lifting state into a parent component is commen when React Components are being refactored.
+//Important Note - State is private to the component that defines it - in this case you cannot update the Board's state from Square
 
-
-function Square({ value }){
-  return <button className='square'>{ value }</button>
+function Square({ value, onSquareClick }){
+  return <button className='square' onClick={onSquareClick}>{ value }</button>
 }
 
 function Board(){
   // Hook that determines state of Child Components
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
   <div className='board'>
     <div className='board-row'>  
       {/* State being passed into Child Components */}
-      <Square marking={squares[0]}/>  
-      <Square marking={squares[1]}/>    
-      <Square marking={squares[2]}/>    
+      <Square marking={squares[0]} onSquareClick={handleClick}/>  
+      <Square marking={squares[1]} onSquareClick={handleClick}/>    
+      <Square marking={squares[2]} onSquareClick={handleClick}/>    
     </div>
     <div className='board-row'>  
-      <Square marking={squares[3]}/>  
-      <Square marking={squares[4]}/>    
-      <Square marking={squares[5]}/>    
+      <Square marking={squares[3]} onSquareClick={handleClick}/>  
+      <Square marking={squares[4]} onSquareClick={handleClick}/>    
+      <Square marking={squares[5]} onSquareClick={handleClick}/>    
     </div>
     <div className='board-row'>  
-      <Square marking={squares[6]}/>  
-      <Square marking={squares[7]}/>    
-      <Square marking={squares[8]}/>    
+      <Square marking={squares[6]} onSquareClick={handleClick}/>  
+      <Square marking={squares[7]} onSquareClick={handleClick}/>    
+      <Square marking={squares[8]} onSquareClick={handleClick}/>    
     </div>
   </div>
   )
