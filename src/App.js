8da1325bccb2c62b456861d9de6c35f6970bf521
Kmolina009 <kevenmolina@gmail.com>
2024-07-02 21:45:41 -0400

    Cleaned up main branch

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-//To check for a winnerm the Board would eed to somehow know the state of each of the 9 Square Component 
-// One could have the board ask each Square component for it's cuirrent State. - this is not recommented(convoluted, prone to bugs)
-//Instead the Board can tell each square what data it should display given a current State - You want the Parent component to store state
-
-
-// To collect data fron multiple children, or have two child components communicate with each other, declare the shared state in 
-// their parent component instead. The parent component can pass that state back down to the children via props. This keeps the 
-// child components in sync with each other and with their parents. 
-
-//Lifting state into a parent component is commen when React Components are being refactored.
-//Important Note - State is private to the component that defines it - in this case you cannot update the Board's state from Square
-
 function Square({ value, onSquareClick }){
   return <button className='square' onClick={onSquareClick}>{ value }</button>
 }
@@ -32,7 +20,7 @@ function Board(){
   return (
   <div className='board'>
     <div className='board-row'>  
-      {/* State being passed into Child Components */}
+      {/* State being passed into Child Components through props*/}
       <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>  
       <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>    
       <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>    
